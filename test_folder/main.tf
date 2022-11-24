cfbb82ff231745234ff9a533949437c6497987b7
metriczulu <stephenson.shane.a@gmail.com>
2022-11-23 23:16:02 -0500

    moving test files over

---
+++ test_folder/main.tf
@@ -0,0 +1,11 @@
+terraform {
+    required_providers =     map "aws" {
+        source = "hashicorp/aws"
+    }    map "aws" {
+        source = "hashicorp/aws"
+    }
+}
+
+provider "aws" {
+    region = "us-east-1a"
+}
---
+++ test_folder/test.tf.py
@@ -0,0 +1,28 @@
+from compose import MetaFormer
+
+
+tf = MetaFormer()
+host = tf.data("aws_ssm_parameter", "dbrx_host", name="dbrx_host")
+token = tf.data("aws_ssm_parameter", "dbrx_token", name="dbrx_token", path=host["name"])
+libs = tf.property("library", location="s3://bucket", entry_point="src.main")
+job = tf.resource(
+    "databricks_job",
+    "dbrx_job",
+    library=libs,
+    name="dbrx_job",
+    host=host["name"],
+    token=token["name"],
+)
+assert set(tf.registry) == {
+    "data.aws_ssm_parameter.dbrx_host",
+    "data.aws_ssm_parameter.dbrx_token",
+    "library",
+    "resource.databricks_job.dbrx_job",
+}
+assert tf.collect() == [host, token, job]
+print(tf._mf_args.name)
+print(tf._mf_dir)
+
+
+if __name__ == "__main__":
+    tf.build()
